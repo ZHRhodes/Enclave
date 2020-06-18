dc4c06cd571ed9d5e2c561b862115e316f6afde8
Zachary Rhodes <zhrhodes@gmail.com>
2020-06-17 22:11:32 -0700

    Initial Commit

---
+++ Enclave/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Enclave
+//
+//  Created by Zachary Rhodes on 6/17/20.
+//  Copyright © 2020 zhrhodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
